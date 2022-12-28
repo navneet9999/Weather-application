e4f5a6b509d2c7ecef7fcb7a38d8ba7d2a73d529
Navneet Yadav <navneet@kgpian.iitkgp.ac.in>
2022-12-28 21:40:47 +0530

    create weather application

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        // SECRET_KEY: "3709f89e826c2838310e77a773533f2d"
+        SECRET_KEY: "d49917eaec737377018e0139c59f505b"
+    }
+}
+
+module.exports = constants;
